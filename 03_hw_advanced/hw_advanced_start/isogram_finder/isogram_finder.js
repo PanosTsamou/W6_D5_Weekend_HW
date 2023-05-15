58194f333bbb6d908100bd7829f4cafa533d6576
Panagiotis <tsamou7@gmail.com>
2023-05-15 01:47:05 +0100

    finish enumeration

--- 03_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
+++ 03_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,17 @@
 const IsogramFinder = function (word) {
-
+    this.word = word
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+ return Array.from(this.word.toLowerCase()).every((char, index , array) => {
+    let counter = 0
+    array.forEach((element) =>{
+        if(element === char){
+            counter += 1
+        }
+    })
+    return counter === 1
+ })
 }
 
 module.exports = IsogramFinder;
--- 03_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
+++ 03_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,10 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.every((char)=> Array.from(this.phrase.toLowerCase().split(/(?:_| )/).join("")).includes(char))
 }
 
 module.exports = PangramFinder;
